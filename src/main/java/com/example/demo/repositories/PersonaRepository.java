40768620b60f12b5226cf8abd7d335de5698f97d
Clara Calidad <sacostaf@unal.edu.co>
2024-07-12 16:02:39 -0500

    Improvement: deleted the old unused classes

--- src/main/java/com/example/demo/repositories/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.restservice.Persona;
-
-public interface PersonaRepository extends CrudRepository<Persona, Integer>{
-
-}
